655d28bea723cb8ce24b696be498030a0db8b44f
apsyes <schaafa@oregonstate.edu>
2025-12-11 06:10:03 -0800

    Update train_ua.py

--- train_ua.py
+++ train_ua.py
@@ -2,18 +2,18 @@ from ultralytics import YOLO
 
 base_weights = "yolov8n.pt"  # or yolov8s.pt for more accuracy
 
-data_yaml = r"C:\Users\you\db\Yolo\UA-DETRAC\data.yaml"
+data_yaml = r"/nfs/stak/users/schaafa/trainingCapstone/data_yaml"
 
 def main():
     model = YOLO(base_weights)
 
     model.train(
         data=data_yaml,
-        epochs=30,        # keep it modest for first run
+        epochs=50,        # keep it modest for first run
         imgsz=1280,
         batch=8,
-        workers=2,        # ThinkPad-friendly
-        patience=5,       # early stop if plateau
+        workers=30,        # ThinkPad-friendly
+        patience=10,       # early stop if plateau
         cos_lr=True,
         plots=True,
     )
